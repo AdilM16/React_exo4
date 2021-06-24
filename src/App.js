4f349a09d7a571d773d8ad36bde6b0a02b55e16b
M16 <Adilmohamedmohamed97@gmail.com>
2021-06-24 14:19:14 +0100

    *

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGem } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let diams = <FontAwesomeIcon icon={faGem} />
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>exo4</h1>
+        <Button> {diams} Mon Bouton</Button>
+      </div>
+    );
+  }
 }
-
-export default App;
